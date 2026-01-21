2472cb0b52d5cc57250cebd5e608ea1ac39022cb
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2026-01-21 19:24:11 -0300

    Update files

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,8 +1,14 @@
 export function add(numbers) {
   let sum = 0;
 
+  if(!numbers) {
+    throw new Error('Numbers not provided.')
+  }
+  
+
   for (const number of numbers) {
-    sum += number;
+    sum += +number;
   }
   return sum;
+  
 }
--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,1 +1,94 @@
-import { test } from 'vitest';
+import { it, expect } from 'vitest';
+import { add } from './math';
+
+it('should summarize all number values in an array', () => {
+
+    // Arrange
+    const numbers = [1, 2, 3];
+    
+    // Act - execute the function
+    const result = add(numbers);
+    
+    // Assert
+    const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + curValue, 0);
+    expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+
+    // Arrange 
+    const numbers = [1, 2, 'a', -1, '1'];
+
+    // Act - execute the function
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+    // Arrange
+    const numbers = ['1', '2', '3'];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert 
+    const expectedResult = numbers.reduce((prevValue, curValue) => +prevValue + +curValue, 0);
+    expect(result).toBe(expectedResult);
+});
+
+it('should yield 0 if an empty array is provided', () => {
+    // Arrange
+    const numbers = [];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBe(0);
+});
+
+it('should yield 0 if an array of spaces is provided', () => {
+    // Arrange
+    const numbers = [' ', ' ', ' '];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBe(0);
+});
+
+it('should yield the provided value if only one value is provided', () => {
+    // Arrange
+    const numbers = [1];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers[0];
+    expect(result).toBe(expectedResult);
+});
+
+it('should yield the provided value if only one value is provided and the others spaces', () => {
+    // Arrange
+    const numbers = [1, ' ', ' ', ' '];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce((prevValue, curValue) => +prevValue + +curValue, 0);
+    expect(result).toBe(expectedResult);
+});
+
+it('should throw an error if no value is passed into the function',() => {
+    // Act
+    const result = add();
+
+    // Assert
+    expect(result).toThrowError('Numbers not provided.');
+});
+
